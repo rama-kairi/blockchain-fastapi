71094a8d081d5170ce368d67ae197a33ce917286
Ramananda Kairi <ramananda.kairi@gmail.ccom>
2022-02-04 13:34:35 +0530

    Blockchain with Python FastAPi

---
+++ main.py
@@ -0,0 +1,48 @@
+import fastapi as _fastapi
+
+import blockchain as _blockchain
+
+blockchain = _blockchain.Blockchain()
+app = _fastapi.FastAPI()
+
+
+# endpoint to mine a block
+@app.post("/mine_block/")
+def mine_block(data: str):
+    if not blockchain.is_chain_valid():
+        return _fastapi.HTTPException(
+            status_code=400, detail="The blockchain is invalid"
+        )
+    return blockchain.mine_block(data=data)
+
+
+# endpoint to return the blockchain
+@app.get("/blockchain/")
+def get_blockchain():
+    if not blockchain.is_chain_valid():
+        return _fastapi.HTTPException(
+            status_code=400, detail="The blockchain is invalid"
+        )
+    return blockchain.chain
+
+
+# endpoint to see if the chain is valid
+@app.get("/validate/")
+def is_blockchain_valid():
+    if not blockchain.is_chain_valid():
+        return _fastapi.HTTPException(
+            status_code=400, detail="The blockchain is invalid"
+        )
+
+    return blockchain.is_chain_valid()
+
+
+# endpoint to return the last block
+@app.get("/blockchain/last/")
+def previous_block():
+    if not blockchain.is_chain_valid():
+        return _fastapi.HTTPException(
+            status_code=400, detail="The blockchain is invalid"
+        )
+
+    return blockchain.get_previous_block()
